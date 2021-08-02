6aea6ed888b03d8b08a121e28078dcd1d67e6f96
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2021-08-02 00:45:04 -0500

    cambios nuevos 2

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -52,7 +52,6 @@
 
     <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     @livewireStyles
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
 </head>
 
