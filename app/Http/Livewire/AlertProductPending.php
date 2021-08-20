18151da7ad1943c2e9f5797fc67c7a67809baede
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2021-08-20 12:40:55 -0500

    pago mercado

--- app/Http/Livewire/AlertProductPending.php
+++ app/Http/Livewire/AlertProductPending.php
@@ -9,7 +9,19 @@ class AlertProductPending extends Component
 {
     public function render()
     {
-        $pendiente = Order::where('status', 1)->where('user_id', auth()->user()->id)->count();
-        return view('livewire.alert-product-pending', compact('pendiente'));
+        // setcookie("productos_pendientes", "Hay productos pendientes", time() - 60);
+        if (auth()->user()) {
+            if (!isset($_COOKIE['productos_pendientes'])) {
+                setcookie("productos_pendientes", "Hay productos pendientes", time() + 3600);
+                $pendiente = Order::where('status', 1)->where('user_id', auth()->user()->id)->count();
+                return view('livewire.alert-product-pending', compact('pendiente'));
+            } else {
+                $pendiente = "";
+                return view('livewire.alert-product-pending', compact('pendiente'));
+            }
+        } else {
+            $pendiente = "";
+            return view('livewire.alert-product-pending', compact('pendiente'));
+        }
     }
 }
--- resources/views/ordenes/orden-payment-page.blade.php
+++ resources/views/ordenes/orden-payment-page.blade.php
@@ -1,4 +1,4 @@
 <x-web-layout>
-    @livewire('payment-paypal', ['orden' => $orden])
-    {{-- @livewire('payment-mercado-pago', ['orden' => $orden]) --}}
+    {{-- @livewire('payment-paypal', ['orden' => $orden]) --}}
+    @livewire('payment-mercado-pago', ['orden' => $orden])
 </x-web-layout>
