4d4ad10c7eae2f4e02dceacec5ac35924c547abe
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2021-09-15 10:57:15 -0500

    cambios htpp

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -12,31 +12,38 @@ use Illuminate\Support\Facades\Auth;
 
 class PaymentController extends Controller
 {
-    public function pagoExitoso(Order $orden, Request $request)
+    // public function pagoExitoso(Order $orden, Request $request)
+    // {
+    //     $this->authorize('payment', $orden);
+    //     $this->authorize('author', $orden);
+
+    //     if ($request->get('payment_id')) {
+    //         $payment_id = $request->get('payment_id');
+
+    //         $reponse = Http::get("https://api.mercadopago.com/v1/payments/$payment_id" . "?access_token=APP_USR-7260790674054640-081218-df6bacdd5688965f92aca54c2782d7ef-806649757");
+    //         $reponse = json_decode($reponse);
+    //         $status = $reponse->status;
+    //         if ($status == 'approved') {
+    //             $items = json_decode($orden->content);
+
+    //             if ($orden->status != 2) {
+    //                 $orden->status = 2;
+    //                 $orden->save();
+    //                 $user = User::where('email', Auth::user()->email)->first();
+    //                 Mail::to($orden->email)->send(new OrdenPagada($user, $orden, $items));
+    //             }
+    //             return view('payment.pago-exitoso-page', compact('orden', 'items'));
+    //         } else {
+    //             return redirect()->route('pagoCancelado', $orden);
+    //         }
+    //     }
+
+    // }
+    public function pagoExitoso(Order $orden)
     {
         $this->authorize('payment', $orden);
         $this->authorize('author', $orden);
 
-        // if ($request->get('payment_id')) {
-        //     $payment_id = $request->get('payment_id');
-
-        //     $reponse = Http::get("https://api.mercadopago.com/v1/payments/$payment_id" . "?access_token=APP_USR-7260790674054640-081218-df6bacdd5688965f92aca54c2782d7ef-806649757");
-        //     $reponse = json_decode($reponse);
-        //     $status = $reponse->status;
-        //     if ($status == 'approved') {
-        //         $items = json_decode($orden->content);
-
-        //         if ($orden->status != 2) {
-        //             $orden->status = 2;
-        //             $orden->save();
-        //             $user = User::where('email', Auth::user()->email)->first();
-        //             Mail::to($orden->email)->send(new OrdenPagada($user, $orden, $items));
-        //         }
-        //         return view('payment.pago-exitoso-page', compact('orden', 'items'));
-        //     } else {
-        //         return redirect()->route('pagoCancelado', $orden);
-        //     }
-        // }
         $items = json_decode($orden->content);
 
         if ($orden->status != 2) {
