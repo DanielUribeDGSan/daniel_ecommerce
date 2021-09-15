973c6ddb89633f13c107a95766b81763f4cd02b0
Daniel Uribe <daniel.uribe.garcia07@gmail.com>
2021-09-15 10:51:45 -0500

    cambios nuevos 2

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,26 +17,35 @@ class PaymentController extends Controller
         $this->authorize('payment', $orden);
         $this->authorize('author', $orden);
 
-        if ($request->get('payment_id')) {
-            $payment_id = $request->get('payment_id');
+        // if ($request->get('payment_id')) {
+        //     $payment_id = $request->get('payment_id');
 
-            $reponse = Http::get("https://api.mercadopago.com/v1/payments/$payment_id" . "?access_token=APP_USR-7260790674054640-081218-df6bacdd5688965f92aca54c2782d7ef-806649757");
-            $reponse = json_decode($reponse);
-            $status = $reponse->status;
-            if ($status == 'approved') {
-                $items = json_decode($orden->content);
+        //     $reponse = Http::get("https://api.mercadopago.com/v1/payments/$payment_id" . "?access_token=APP_USR-7260790674054640-081218-df6bacdd5688965f92aca54c2782d7ef-806649757");
+        //     $reponse = json_decode($reponse);
+        //     $status = $reponse->status;
+        //     if ($status == 'approved') {
+        //         $items = json_decode($orden->content);
 
-                if ($orden->status != 2) {
-                    $orden->status = 2;
-                    $orden->save();
-                    $user = User::where('email', Auth::user()->email)->first();
-                    Mail::to($orden->email)->send(new OrdenPagada($user, $orden, $items));
-                }
-                return view('payment.pago-exitoso-page', compact('orden', 'items'));
-            } else {
-                return redirect()->route('pagoCancelado', $orden);
-            }
+        //         if ($orden->status != 2) {
+        //             $orden->status = 2;
+        //             $orden->save();
+        //             $user = User::where('email', Auth::user()->email)->first();
+        //             Mail::to($orden->email)->send(new OrdenPagada($user, $orden, $items));
+        //         }
+        //         return view('payment.pago-exitoso-page', compact('orden', 'items'));
+        //     } else {
+        //         return redirect()->route('pagoCancelado', $orden);
+        //     }
+        // }
+        $items = json_decode($orden->content);
+
+        if ($orden->status != 2) {
+            $orden->status = 2;
+            $orden->save();
+            $user = User::where('email', Auth::user()->email)->first();
+            Mail::to($orden->email)->send(new OrdenPagada($user, $orden, $items));
         }
+        return view('payment.pago-exitoso-page', compact('orden', 'items'));
     }
 
     public function pagoPendiente(Order $orden)
